045af85c3c6c2537b7b9600c80ad2682bcbee8c3
rtrinu <rbinu2004@gmail.com>
2025-02-19 17:03:41 +0000

    initial layout of home page created

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+"""
 @app.route('/predict',methods=['POST'])
 def predict():
     stock_symbol = request.form.get('stock_symbol')
 
     stock = Stock(stock_symbol)
-    
+""" 
 
 if __name__ == '__main__':
     app.run(debug=True)
